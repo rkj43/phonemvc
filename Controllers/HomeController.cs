90a5d2e88aaceac05c4b924ab8bc7030c05ee0c9
rkj43 <rahulkjohn43@gmail.com>
2020-01-09 18:52:18 +0530

    hardcodeddata working now.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,25 +8,29 @@ using phonemvc.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net;
-using PhoneBook;
+using phonebookghp;
 
 namespace phonemvc.Controllers
 {
     public class HomeController : Controller
     {
+        
+        
         public IActionResult Index()
         {
+            
             return View();
         }
 
         
         public IActionResult Books()
         {
-            
-           PhoneBook.addContact(fileHandler.readContactsFromJson());
-          
+           var json = "{\"books\":[{\"title\":\"Book 1\", \"author\":\"Author1\", \"price\":762, \"inStock\":15},{\"title\":\"Book 2\", \"author\":\"Author2\", \"price\":100, \"inStock\":1},{\"title\":\"Book 3\", \"author\":\"Author3\", \"price\":185.5, \"inStock\":5},{\"title\":\"Book 4\", \"author\":\"Author 4\", \"price\":1748, \"inStock\":3},{\"title\":\"Book 5\", \"author\":\"Author 5\", \"price\":999, \"inStock\":20},{\"title\":\"Book 6\", \"author\":\"Author 6\", \"price\":499.5, \"inStock\":3},{\"title\":\"Book 7\", \"author\":\"Author 7\", \"price\":564.5, \"inStock\":0}]}";			
+			
+            var books = JsonConvert.DeserializeObject<Books>(json);
 
-             return View();
+
+             return View(books);
         }
 
         public IActionResult Error()
